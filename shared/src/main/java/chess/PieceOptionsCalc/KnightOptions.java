3e26bda0c86abf2f7af5444596382423734d11f8
huntyGwell <huntergruwell@gmail.com>
2024-09-20 16:20:08 -0600

    have a good knight

--- shared/src/main/java/chess/PieceOptionsCalc/KnightOptions.java
+++ shared/src/main/java/chess/PieceOptionsCalc/KnightOptions.java
@@ -9,4 +9,8 @@ import java.util.HashSet;
 
 
 public class KnightOptions implements PieceOptionsCalc {
+    public static HashSet<ChessMove> calculateOptions(ChessBoard chessBoard, ChessPosition position) {
+        int[][] moves = {{-2,1},{-1,2},{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1}};
+        return PieceOptionsCalc.CreateStateMoves(chessBoard, position, moves);
+    }
 }
