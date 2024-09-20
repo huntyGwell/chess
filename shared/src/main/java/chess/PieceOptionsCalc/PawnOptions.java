2cd4c29520583d069c5a1a7d22c99c8910e70799
huntyGwell <huntergruwell@gmail.com>
2024-09-20 17:38:21 -0600

    i am dying

--- shared/src/main/java/chess/PieceOptionsCalc/PawnOptions.java
+++ shared/src/main/java/chess/PieceOptionsCalc/PawnOptions.java
@@ -9,4 +9,20 @@ import chess.ChessPiece;
 import java.util.HashSet;
 
 public class PawnOptions implements PieceOptionsCalc{
+    //this is actually harder than the others!
+    public static HashSet<ChessMove> calculateOptions(ChessBoard board, ChessPosition position){
+        HashSet<ChessMove> moves = HashSet.newHashSet(17);
+        int row =position.getRow();
+        int column = position.getColumn();
+        ChessPiece.PieceType[] promo = new ChessPiece.PieceType[]{null};
+        //attack right
+
+        //attack left
+
+        //doublejump first move
+
+        return moves;
+    }
+
+
 }
