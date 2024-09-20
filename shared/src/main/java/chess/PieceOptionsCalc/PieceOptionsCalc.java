099085cd7f09f189da2058de584794b5bb270fa1
huntyGwell <huntergruwell@gmail.com>
2024-09-20 13:40:08 -0600

    oops i did it again i broke more stuff

--- shared/src/main/java/chess/PieceOptionsCalc/PieceOptionsCalc.java
+++ shared/src/main/java/chess/PieceOptionsCalc/PieceOptionsCalc.java
@@ -14,7 +14,7 @@ public interface PieceOptionsCalc {
     //get Options
         //return null (implement in the Options * classes)
     static HashSet<ChessMove> GetOptions(ChessBoard board, ChessPosition Position) {
-        return null;
+        return null; //I want to spend more time understanding java
     }
 
     //isValid position
@@ -24,11 +24,14 @@ public interface PieceOptionsCalc {
     }
 
     //static option
-    static HashSet<ChessMove> CreateStateMoves(ChessBoard board, ChessPosition position) {
+    static HashSet<ChessMove> CreateStateMoves(ChessBoard board, ChessPosition position, int[][] realizableMoves) {
         HashSet<ChessMove> moves = HashSet.newHashSet(8);
         int row = position.getRow();
         int column = position.getColumn();
-        //ChessGame.
+        ChessGame.TeamColor teamColor = board.getTeamColor(position);
+        for(int i = 0; i < realizableMoves.length; i++) {
+            ChessPosition newPosition = new ChessPosition(row, column);//modify with realizableMoves
+        }
     }
     //direction option
     static HashSet<ChessMove> CreateDirectionMoves(ChessBoard board, ChessPosition position) {
