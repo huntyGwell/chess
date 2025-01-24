665f1fb4211c515c32cfbeb69fdad2fc2228dae3
huntyGwell <huntergruwell@gmail.com>
2025-01-24 16:05:12 -0700

    i am a dang queen

--- shared/src/main/java/chess/PieceMoveCalc/QueenCalc.java
+++ shared/src/main/java/chess/PieceMoveCalc/QueenCalc.java
@@ -1,4 +1,20 @@
 package chess.PieceMoveCalc;
 
-public class QueenCalc {
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.HashSet;
+
+public class QueenCalc implements PieceMoveCalc{
+    public static HashSet<ChessMove> getMoves(ChessBoard board, ChessPosition position) {
+        int x = position.getCol();
+        int y = position.getRow();
+        int[][] moves = {{-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}};
+
+        ChessGame.TeamColor team = board.getSquaresTeam(position);
+
+        return PieceMoveCalc.dynamicMoves(board, position, moves, y, x, team);
+    }
 }
