6c3500e307d514a8a585e178b811aa4e4c669cae
huntyGwell <huntergruwell@gmail.com>
2025-03-14 16:13:54 -0600

    setting up services

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,13 @@
 package service;
 
+import dataAccess.*;
+import model.*;
+
+import java.util.UUID;
+
 public class UserService {
+
+    UserDAO userDAO;
+    GameDAO gameDAO;
+    AuthDAO authDAO;
 }
