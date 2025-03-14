37884ab66bcb3ac8fee057d1ef19c6eb4de72aa6
huntyGwell <huntergruwell@gmail.com>
2025-03-14 16:33:55 -0600

    really messed up authDAO oops

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,3 +1,22 @@
 package model;
 
-public record AuthData(String authToken, String username) {}
+import model.AuthData;
+
+import java.util.HashSet;
+
+public record AuthData(String authToken, String username) {
+
+    HashSet<AuthData> db;
+
+    public AuthData() {
+        db = new HashSet<>();
+    }
+
+    void addAuthData(AuthData authData) {}
+
+    void removeAuthData(String authToken) {}
+
+    AuthData getAuthData(String authToken) {}
+
+    void clear();
+}
