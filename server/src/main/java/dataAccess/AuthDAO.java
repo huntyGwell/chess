eb05b6220f34ae75d3083811858b0ccef7360b19
huntyGwell <huntergruwell@gmail.com>
2025-03-19 12:19:59 -0600

    planning out how to catch up to deadlines

--- server/src/main/java/dataAccess/AuthDAO.java
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -1,5 +1,9 @@
 package dataAccess;
 
+//I got stumped on phase 3 and am now crazy far behind as a result I am now working on phase 4 too.(also late on that...oops)
+//It seems like this should actually be an interface and I will build the use-ability out in classes for phase 3 and also
+//for phase 4
+
 import model.AuthData;
 import java.util.HashMap;
 import java.util.Map;
