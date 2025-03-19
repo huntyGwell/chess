7ca30c135a6324185ac38e662fea94504c215268
huntyGwell <huntergruwell@gmail.com>
2025-03-19 12:34:03 -0600

    need to build out exceptions

--- server/src/main/java/dataAccess/AuthDAO.java
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -5,25 +5,11 @@ package dataAccess;
 //for phase 4
 
 import model.AuthData;
-import java.util.HashMap;
-import java.util.Map;
 
-public class AuthDAO {
-    private final Map<String, AuthData> authTokens = new HashMap<>();
-
-    public void createAuth(AuthData auth) {
-        authTokens.put(auth.authToken(), auth);
-    }
-
-    public AuthData getAuth(String authToken) {
-        return authTokens.get(authToken);
-    }
-
-    public void deleteAuth(String authToken) {
-        authTokens.remove(authToken);
-    }
-
-    public void clear() {
-        authTokens.clear();
-    }
+public interface AuthDAO {
+    void addAuthData(AuthData authData);
+    void removeAuthData(AuthData authData);
+    AuthData getAuthData(String authToken);// throws DataAccessException;
+    void clearAuthData();
 }
+//need to build out exceptions
