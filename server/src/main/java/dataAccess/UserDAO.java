8ce5a55e11cacbab0e43acc075250431b1db4eec
huntyGwell <huntergruwell@gmail.com>
2025-03-19 15:01:18 -0600

    memory user dao

--- server/src/main/java/dataAccess/UserDAO.java
+++ server/src/main/java/dataAccess/UserDAO.java
@@ -3,9 +3,9 @@ package dataAccess;
 import model.UserData;
 
 public interface UserDAO {
-    void createUser(UserData user);//throws DataAccessException
-    UserData getUser(String userName);//throws DataAccessException
-    boolean verifyUser(String userName, String password);//throws DataAccessException
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String userName) throws DataAccessException;
+    boolean verifyUser(String userName, String password) throws DataAccessException;
     void clear();
 }
 // build out exceptions
--- server/src/main/java/dataAccess/memory/MemoryUserDAO.java
+++ server/src/main/java/dataAccess/memory/MemoryUserDAO.java
@@ -1,4 +1,22 @@
 package dataAccess.memory;
 
-public class MemoryUserDAO {
+import dataAccess.UserDAO;
+import model.UserData;
+
+import java.util.HashSet;
+
+public class MemoryUserDAO implements UserDAO {
+
+    private HashSet<UserData> userSet;
+    public MemoryUserDAO() {userSet = new HashSet<>(32);}
+
+    @Override
+    public UserData getUser(String userName) {}
+    @Override
+    public void createUser(UserData user) {}
+    @Override
+    public boolean verifyUser(String userName, String password) {}
+    @Override
+    public void clear() {}
+
 }
