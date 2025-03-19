041489d9a6d6be77dda681831b5c627e6a596b51
huntyGwell <huntergruwell@gmail.com>
2025-03-19 12:48:36 -0600

    gameDAO interface

--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -1,4 +1,14 @@
 package dataAccess;
 
+import model.GameData;
+
+import java.util.HashSet;
+
 public interface GameDAO {
+    HashSet<GameData> getAllGames();
+    void makeGame(GameData game); ////throws DataAccessException
+    void updateGame(GameData game); ////throws DataAccessException
+    GameData getGame(String gameId); ////throws DataAccessException
+    boolean validateGame(int gameId);
+    void clearGames();
 }
