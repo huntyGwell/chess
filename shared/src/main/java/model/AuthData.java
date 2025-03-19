539dd597234b919cd1b1c908a8d02ff710fcf4e3
huntyGwell <huntergruwell@gmail.com>
2025-03-19 00:07:28 -0600

    fix auth modle

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,29 +1,3 @@
 package model;
 
-import model.AuthData;
-
-import java.util.HashSet;
-
-public record AuthData(String authToken, String username) {
-
-    HashSet<AuthData> db;
-
-    public AuthData() {
-        db = new HashSet<>();
-    }
-
-    void addAuthData(AuthData authData) {}
-
-    void removeAuthData(String authToken) {}
-
-    AuthData getAuthData(String authToken) {}
-
-    void clear();
-}
-///////////////
-//////////////
-/////////////
-//all of this will need to be re written
-////////////
-///////////
-///////////
+public record AuthData(String authToken, String username) {}
