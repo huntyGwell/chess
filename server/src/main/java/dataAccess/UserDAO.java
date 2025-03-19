41264f4330268dc28e9581da044a2ea02c5c60e0
huntyGwell <huntergruwell@gmail.com>
2025-03-19 12:39:48 -0600

    userDAO interface

--- server/src/main/java/dataAccess/UserDAO.java
+++ server/src/main/java/dataAccess/UserDAO.java
@@ -1,4 +1,10 @@
 package dataAccess;
 
+import model.UserData;
+
 public interface UserDAO {
+    void createUser(UserData user);
+    UserData getUser(String userName);
+    boolean verifyUser(String userName, String password);
+    void clear();
 }
