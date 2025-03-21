5c2491362c6ee6ef1543067945d10298975c81f7
huntyGwell <huntergruwell@gmail.com>
2025-03-21 14:43:45 -0600

    ,memory authDAO setup

--- server/src/main/java/dataAccess/memory/MemoryAuthDAO.java
+++ server/src/main/java/dataAccess/memory/MemoryAuthDAO.java
@@ -1,4 +1,15 @@
 package dataAccess.memory;
 
-public class MemoryAuthDAO {
+import dataAccess.DataAccessException;
+import dataAccess.AuthDAO;
+import model.AuthData;
+
+import java.util.HashSet;
+
+public class MemoryAuthDAO implements AuthDAO {
+    private HashSet<AuthData> authDataSet;
+
+    public MemoryAuthDAO() {
+        authDataSet = new HashSet<>();
+    }
 }
