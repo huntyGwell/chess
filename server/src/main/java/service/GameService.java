6933640e54edf000cda6011d547946a9ea9c8176
huntyGwell <huntergruwell@gmail.com>
2025-03-22 06:41:42 -0600

    ,game services clear

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -6,4 +6,17 @@ import chess.ChessGame;
 import chess.ChessBoard;
 
 public class GameService {
+    GameDAO gameDAO;
+    UserDAO userDAO;
+    AuthDAO authDAO;
+
+    public GameService(GameDAO gameDAO, UserDAO userDAO, AuthDAO authDAO) {
+        this.gameDAO = gameDAO;
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public void clear() {
+        gameDAO.clearGames();
+    }
 }
